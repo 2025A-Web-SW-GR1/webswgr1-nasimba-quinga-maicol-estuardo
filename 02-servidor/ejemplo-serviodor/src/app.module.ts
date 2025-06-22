bffa57acddd146f657c90e5181e2b78bd9633fcb
Maicol Nasimba <maicol.nasimba@epn.edu.ec>
2025-06-21 19:16:29 -0500

    Examen01_Maicol Nasimba

--- 02-servidor/ejemplo-serviodor/src/app.module.ts
+++ 02-servidor/ejemplo-serviodor/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CasaController } from './Examen01/casa.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CasaController],
   providers: [AppService],
 })
 export class AppModule {}
